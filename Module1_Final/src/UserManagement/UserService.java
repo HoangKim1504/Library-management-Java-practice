7c1d4ec55adf340c359bcd85466dd7663d81a84f
Hoang Kim Tran <thkim1504@gmail.com>
2026-04-19 13:06:25 +0700

    Add logout function

--- Module1_Final/src/UserManagement/UserService.java
+++ Module1_Final/src/UserManagement/UserService.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package UserManagement;
 
 import java.util.ArrayList;
@@ -25,3 +26,36 @@ public class UserService {
         return null;
     }
 }
+=======
+package UserManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private final List<User> userList = new ArrayList<>(); // Prevents accidental reassignment to the list
+
+    public void createUser(User initData) {
+        userList.add(initData);
+    }
+
+    public List<User> getAllUsers() {
+        return new ArrayList<>(userList); // return copy so that outside cannot modify the internal list
+    }
+
+    public User login(String userName, String password, String userId) {
+        for (User user: userList) {
+            if (user.getUserName().equals(userName)
+                    && user.getPassword().equals(password)
+                    && userId == null) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public String logout(String userId) {
+        return null;
+    }
+}
+>>>>>>> c4c41e1 (Add logout function)
