e736dbd72dbb3a6ad403a6e911993da0e5fc4530
Hoang Kim Tran <thkim1504@gmail.com>
2026-05-08 00:00:13 +0700

    Rename folders: util -> enums

---
+++ Module1_Final/src/enums/Status.java
@@ -0,0 +1,18 @@
+package enums;
+
+public enum Status {
+    ACTIVATED("Hoạt động"),
+    BLOCK("Khoá"),
+    OTHER("Khác"),
+    NONE("");
+
+    private final String displayName;
+
+    Status(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
---
+++ Module1_Final/src/enums/UserType.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum UserType {
+    ADMIN("Quản trị viên"),
+    MANAGER("Quản lý"),
+    USER("Người dùng"),
+    OTHER("Khác"),
+    NONE("");
+
+    private final String displayName;
+
+    UserType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
